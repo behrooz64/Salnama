04c91ec02a3f840b7a0e9e3369c09f3d649931bf
behrooz64 <91674833+behrooz64@users.noreply.github.com>
2026-09-05 15:36:47 +0330

    Improve PWA cache update handling

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='salnama-v2';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='salnama-v3';
+const ASSETS=['./','./index.html','./style.css','./app.js','./animalData.js','./manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
