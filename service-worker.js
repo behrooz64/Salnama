6e20f8b84d8cbd3601b25443d896851e3f48615d
behrooz64 <91674833+behrooz64@users.noreply.github.com>
2026-09-05 15:45:19 +0330

    Improve PWA cache update strategy

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,29 @@
-const CACHE='salnama-v3';
+const CACHE='salnama-v4';
 const ASSETS=['./','./index.html','./style.css','./app.js','./animalData.js','./manifest.json'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(response=>{
+        if(e.request.method==='GET'){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(e.request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
